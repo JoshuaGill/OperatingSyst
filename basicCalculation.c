1454245366e3e08bc7ccd99d2785874c39f9adcb
admin <admin@j109-32.isd.glam.ac.uk>
2015-01-06 17:44:46 +0000

    Second commit

---
+++ basicCalculation.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int a,b,c;
+
+int main()
+{
+	printf("Enter a number ");
+	scanf("%d", &a);
+
+	printf("Enter a second number ");
+	scanf("%d", &b);
+
+	c = a + b;
+
+	printf("Added = %d\n", c);
+
+}
